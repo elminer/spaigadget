596170fa897349e5edf71f965a9f6f7fee215e63
elminer <forces.unknown@c64ownersclub.pl>
2016-01-12 15:41:41 -0500

    First test of packaged ships

--- spaitable.js
+++ spaitable.js
@@ -1,19 +1,301 @@
 $(function() {
-  var COLLAT_AMOUNT = 0.02;
-  var A2_COST = 250;
-  var UVHO_COST = 350;
-  var LOCAL_COST = 100;
+  $.PACKAGED_VOLUMES = {
+    "Bantam": 2500, 
+    "Condor": 2500, 
+    "Griffin": 2500, 
+    "Slasher": 2500, 
+    "Probe": 2500, 
+    "Rifter": 2500, 
+    "Reaper": 2500, 
+    "Executioner": 2500, 
+    "Inquisitor": 2500, 
+    "Tormentor": 2500, 
+    "Navitas": 2500, 
+    "Tristan": 2500, 
+    "Incursus": 2500, 
+    "Impairor": 2500, 
+    "Punisher": 2500, 
+    "Breacher": 2500, 
+    "Burst": 2500, 
+    "Ibis": 2500, 
+    "Kestrel": 2500, 
+    "Merlin": 2500, 
+    "Heron": 2500, 
+    "Velator": 2500, 
+    "Imicus": 2500, 
+    "Atron": 2500, 
+    "Maulus": 2500, 
+    "Echo": 2500, 
+    "Osprey": 10000, 
+    "Caracal": 10000, 
+    "Stabber": 10000, 
+    "Moa": 10000, 
+    "Maller": 10000, 
+    "Augoror": 10000, 
+    "Vexor": 10000, 
+    "Thorax": 10000, 
+    "Arbitrator": 10000, 
+    "Rupture": 10000, 
+    "Bellicose": 10000, 
+    "Scythe": 10000, 
+    "Blackbird": 10000, 
+    "Celestis": 10000, 
+    "Exequror": 10000, 
+    "Raven": 50000, 
+    "Tempest": 50000, 
+    "Scorpion": 50000, 
+    "Megathron": 50000, 
+    "Apocalypse": 50000, 
+    "Armageddon": 50000, 
+    "Typhoon": 50000, 
+    "Dominix": 50000, 
+    "Badger": 20000, 
+    "Tayra": 20000, 
+    "Nereus": 20000, 
+    "Hoarder": 20000, 
+    "Mammoth": 20000, 
+    "Wreathe": 20000, 
+    "Kryos": 20000, 
+    "Epithal": 20000, 
+    "Miasmos": 20000, 
+    "Iteron Mark V": 20000, 
+    "Erebus": 10000000, 
+    "Caldari Shuttle": 500, 
+    "Bestower": 20000, 
+    "Omen": 10000, 
+    "Zephyr": 2500, 
+    "Crucifier": 2500, 
+    "Utu": 2500, 
+    "Adrestia": 10000, 
+    "Primae": 20000, 
+    "Noctis": 20000, 
+    "Revenant": 1000000, 
+    "Malice": 2500, 
+    "Vangel": 10000, 
+    "Echelon": 2500, 
+    "Gnosis": 15000, 
+    "Leviathan": 10000000, 
+    "Vigil": 2500, 
+    "Scorpion Ishukone Watch": 50000, 
+    "Oracle": 15000, 
+    "Naga": 15000, 
+    "Talos": 15000, 
+    "Tornado": 15000, 
+    "Miasmos Quafe Ultra Edition": 20000, 
+    "Miasmos Quafe Ultramarine Edition": 20000, 
+    "Guardian-Vexor": 10000, 
+    "Gallente Shuttle": 500, 
+    "Minmatar Shuttle": 500, 
+    "Amarr Shuttle": 500, 
+    "Helios": 2500, 
+    "Keres": 2500, 
+    "Crow": 2500, 
+    "Raptor": 2500, 
+    "Cheetah": 2500, 
+    "Crusader": 2500, 
+    "Malediction": 2500, 
+    "Anathema": 2500, 
+    "Sentinel": 2500, 
+    "Buzzard": 2500, 
+    "Kitsune": 2500, 
+    "Claw": 2500, 
+    "Stiletto": 2500, 
+    "Taranis": 2500, 
+    "Ares": 2500, 
+    "Vengeance": 2500, 
+    "Wolf": 2500, 
+    "Nemesis": 2500, 
+    "Hawk": 2500, 
+    "Harpy": 2500, 
+    "Hyena": 2500, 
+    "Retribution": 2500, 
+    "Jaguar": 2500, 
+    "Avatar": 10000000, 
+    "Apocalypse Imperial Issue": 50000, 
+    "Armageddon Imperial Issue": 50000, 
+    "Gold Magnate": 2500, 
+    "Silver Magnate": 2500, 
+    "Falcon": 10000, 
+    "Rook": 10000, 
+    "Huginn": 10000, 
+    "Rapier": 10000, 
+    "Pilgrim": 10000, 
+    "Arazu": 10000, 
+    "Lachesis": 10000, 
+    "Guardian": 10000, 
+    "Oneiros": 10000, 
+    "Cerberus": 10000, 
+    "Onyx": 10000, 
+    "Vagabond": 10000, 
+    "Zealot": 10000, 
+    "Eagle": 10000, 
+    "Muninn": 10000, 
+    "Devoter": 10000, 
+    "Sacrilege": 10000, 
+    "Phobos": 10000, 
+    "Deimos": 10000, 
+    "Manticore": 2500, 
+    "Hound": 2500, 
+    "Ishkur": 2500, 
+    "Enyo": 2500, 
+    "Crane": 20000, 
+    "Bustard": 20000, 
+    "Prorator": 20000, 
+    "Prowler": 20000, 
+    "Viator": 20000, 
+    "Occator": 20000, 
+    "Mastodon": 20000, 
+    "Impel": 20000, 
+    "Megathron Federate Issue": 50000, 
+    "Ferox": 15000, 
+    "Brutix": 15000, 
+    "Cyclone": 15000, 
+    "Prophecy": 15000, 
+    "Coercer": 5000, 
+    "Cormorant": 5000, 
+    "Catalyst": 5000, 
+    "Thrasher": 5000, 
+    "Covetor": 3750, 
+    "Retriever": 3750, 
+    "Procurer": 3750, 
+    "Caldari Navy Hookbill": 2500, 
+    "Caracal Navy Issue": 10000, 
+    "Raven Navy Issue": 50000, 
+    "Imperial Navy Slicer": 2500, 
+    "Omen Navy Issue": 10000, 
+    "Gila": 10000, 
+    "Phantasm": 10000, 
+    "Cynabal": 10000, 
+    "Vigilant": 10000, 
+    "Apocalypse Navy Issue": 50000, 
+    "Megathron Navy Issue": 50000, 
+    "Tempest Fleet Issue": 50000, 
+    "Nightmare": 50000, 
+    "Machariel": 50000, 
+    "Vindicator": 50000, 
+    "Republic Fleet Firetail": 2500, 
+    "Federation Navy Comet": 2500, 
+    "Vexor Navy Issue": 10000, 
+    "Rattlesnake": 50000, 
+    "Bhaalgorn": 50000, 
+    "Ashimmu": 10000, 
+    "Succubus": 2500, 
+    "Cruor": 2500, 
+    "Daredevil": 2500, 
+    "Worm": 2500, 
+    "Dramiel": 2500, 
+    "Revelation": 1000000, 
+    "Naglfar": 1000000, 
+    "Moros": 1000000, 
+    "Phoenix": 1000000, 
+    "Sigil": 20000, 
+    "Curse": 10000, 
+    "Providence": 1000000, 
+    "Charon": 1000000, 
+    "Obelisk": 1000000, 
+    "Fenrir": 1000000, 
+    "Goru's Shuttle": 500, 
+    "Guristas Shuttle": 500, 
+    "Redeemer": 50000, 
+    "Sin": 50000, 
+    "Widow": 50000, 
+    "Panther": 50000, 
+    "Eos": 15000, 
+    "Sleipnir": 15000, 
+    "Vulture": 15000, 
+    "Absolution": 15000, 
+    "Heretic": 5000, 
+    "Eris": 5000, 
+    "Flycatcher": 5000, 
+    "Astarte": 15000, 
+    "Claymore": 15000, 
+    "Nighthawk": 15000, 
+    "Damnation": 15000, 
+    "Hulk": 3750, 
+    "Skiff": 3750, 
+    "Mackinaw": 3750, 
+    "Hel": 1000000, 
+    "Archon": 1000000, 
+    "Ragnarok": 10000000, 
+    "Thanatos": 1000000, 
+    "Nyx": 1000000, 
+    "Chimera": 1000000, 
+    "Wyvern": 1000000, 
+    "Aeon": 1000000, 
+    "Nidhoggur": 1000000, 
+    "Rokh": 50000, 
+    "Hyperion": 50000, 
+    "Abaddon": 50000, 
+    "Maelstrom": 50000, 
+    "Harbinger": 15000, 
+    "Drake": 15000, 
+    "Myrmidon": 15000, 
+    "Hurricane": 15000, 
+    "Raven State Issue": 50000, 
+    "Tempest Tribal Issue": 50000, 
+    "Rorqual": 1000000, 
+    "Orca": 500000, 
+    "Paladin": 50000, 
+    "Kronos": 50000, 
+    "Vargur": 50000, 
+    "Golem": 50000, 
+    "Rhea": 1000000, 
+    "Nomad": 1000000, 
+    "Anshar": 1000000, 
+    "Ark": 1000000, 
+    "Magnate": 2500, 
+    "Apotheosis": 500, 
+    "Scythe Fleet Issue": 10000, 
+    "Augoror Navy Issue": 10000, 
+    "Osprey Navy Issue": 10000, 
+    "Exequror Navy Issue": 10000, 
+    "Tengu": 10000, 
+    "Legion": 5000, 
+    "Proteus": 5000, 
+    "Loki": 5000, 
+    "Interbus Shuttle": 500, 
+    "Freki": 2500, 
+    "Mimir": 10000, 
+    "Armageddon Navy Issue": 50000, 
+    "Dominix Navy Issue": 50000, 
+    "Scorpion Navy Issue": 50000, 
+    "Typhoon Fleet Issue": 50000, 
+    "Cambion": 2500, 
+    "Etana": 10000, 
+    "Miasmos Amastris Edition": 20000, 
+    "InterBus Catalyst": 5000, 
+    "Intaki Syndicate Catalyst": 5000, 
+    "Inner Zone Shipping Catalyst": 5000, 
+    "Quafe Catalyst": 5000, 
+    "Aliastra Catalyst": 5000, 
+    "Algos": 5000, 
+    "Dragoon": 5000, 
+    "Corax": 5000, 
+    "Talwar": 5000, 
+    "Venture": 2500, 
+    "Sukuuvestaa Heron": 2500, 
+    "Inner Zone Shipping Imicus": 2500, 
+    "Sarum Magnate": 2500, 
+    "Vherokior Probe": 2500, 
+    "Hematos": 2500, 
+    "Taipan": 2500, 
+    "Violator": 2500, 
+    "Nefantar Thrasher": 5000, 
+    "Brutix Navy Issue": 15000, 
+    "Drake Navy Issue": 15000, 
+    "Harbinger Navy Issue": 15000, 
+    "Hurricane Fleet Issue": 15000, 
+    "Tash-Murkon Magnate": 2500
+  };
+  $.COLLAT_AMOUNT = 0.02;
+  $.A2_COST = 250;
+  $.UVHO_COST = 350;
+  $.LOCAL_COST = 100;
 
-  var sellValue = parseFloat($('th .nowrap:nth-child(1)').html().replace(/,/g,''));
-  var buyValue = parseFloat($('th :nth-child(3)').html().replace(/,/g,''));
-  var rawVolume = $('th :nth-child(5)').text();
-  var volume = parseFloat(rawVolume.substr(0, rawVolume.length - 2).replace(/[^\d\.]/g,''));
-  var sellCollat = sellValue * COLLAT_AMOUNT;
-  var buyCollat = buyValue * COLLAT_AMOUNT;
-
+  // Put the commas back when we click on something else
   $.formatNumber = function (str) { 
-    x = str.split('.'); 
-    x1 = x[0]; x2 = x.length > 1 ? '.' + x[1] : ''; 
+    var x = str.split('.'); 
+    var x1 = x[0]; x2 = x.length > 1 ? '.' + x[1] : ''; 
     var rgx = /(\d+)(\d{3})/; 
     while (rgx.test(x1)) { 
         x1 = x1.replace(rgx, '$1' + ',' + '$2'); 
@@ -21,29 +303,92 @@ $(function() {
     return x1 + x2;
   };
 
+  // Decimal points, but nothing else for simple copy-pasting
   $.formatNumberToString = function (num) {
     return $.formatNumber(num.toFixed(2).toString());
+  };
+
+  $.shipmentVolume = function() {
+    var rawVolume = $('th :nth-child(5)').text();
+    return parseFloat(rawVolume.substr(0, rawVolume.length - 2).replace(/[^\d\.]/g,''));
   }
 
-  $('#a2NoCollatReward').text($.formatNumberToString(volume * A2_COST));
-  $('#a2SellReward').text($.formatNumberToString(volume * A2_COST + sellCollat));
-  $('#a2BuyReward').text($.formatNumberToString(volume * A2_COST + buyCollat));
+  //Check for packageable ships
+  $.isPackageableShips = function() {
+    return $('.line-item-row').is(function(i, v) {
+      return $.itemIsPackable($(v).children(":nth-child(2)").text().trim());
+    });
+  };
 
-  $('#uvhoNoCollatReward').text($.formatNumberToString(volume * UVHO_COST));
-  $('#uvhoSellReward').text($.formatNumberToString(volume * UVHO_COST + sellCollat));
-  $('#uvhoBuyReward').text($.formatNumberToString(volume * UVHO_COST + buyCollat));
+  $.itemIsPackable = function(name) {
+    return $.PACKAGED_VOLUMES.hasOwnProperty(name);
+  };
 
-  $('#localNoCollatReward').text($.formatNumberToString(volume * LOCAL_COST));
-  $('#localSellReward').text($.formatNumberToString(volume * LOCAL_COST + sellCollat));
-  $('#localBuyReward').text($.formatNumberToString(volume * LOCAL_COST + buyCollat));
+  //Calculate packaged ship volumes and update the results
+  $.computePackagedVolumes = function() {
+    var totalunPackagedVolume = 0.0;
+    var totalPackagedVolume = 0.0;
+    $('.line-item-row').each(function(i, v) {
+      var shipName = $(v).children(":nth-child(2)").text().trim();
+      if($.itemIsPackable(shipName)) {
+        var quantity = parseFloat($(v).children(":nth-child(1)").text().replace(/,/g,""));
+        var unPackagedVolume = parseFloat($(v).children(":nth-child(3)").text().replace(/,/g,""));
+        var packagedVolume = $.PACKAGED_VOLUMES[shipName];
+        totalunPackagedVolume += unPackagedVolume * quantity;
+        totalPackagedVolume += packagedVolume * quantity;
 
-  $('.sellAmount').each(function(i, val){
-    $(val).text($.formatNumberToString(sellValue));
-  });
+        var sellAmount = parseFloat($(v).children(":nth-child(4)").children(":nth-child(1)").text().replace(/,/g,""));
+        var buyAmount = parseFloat($(v).children(":nth-child(4)").children(":nth-child(3)").text().replace(/,/g,""));
+        var iskDensitySell = sellAmount/packagedVolume;
+        var iskDensityBuy =  buyAmount/packagedVolume;
+  
 
-  $('.buyAmount').each(function(i, val){
-    $(val).text($.formatNumberToString(buyValue));
-  });
+        //Replace volume
+        $(v).children(":nth-child(3)").text($.formatNumberToString(packagedVolume));
+        //Replace density
+        $(v).children(":nth-child(6)").html($.formatNumberToString(iskDensitySell) + "<br/>" + $.formatNumberToString(iskDensityBuy));
+      }
+    });
+    //Update total volume
+    var totalVolume = $.shipmentVolume() - totalunPackagedVolume + totalPackagedVolume;
+    $('th :nth-child(5)').html($.formatNumberToString(totalVolume) + "m<sup>3</sup>");
+    //Update the warning
+    $('#ships-warning h4').html("PACKAGED SHIPS");
+    //Hide the button
+    $('#package-toggle').hide();
+
+    //Recompute the costs
+    $.computeShippingCosts();
+  };
+
+  // Compute shipping costs based on the volume and pricing currently in the results
+  $.computeShippingCosts = function() {
+    var sellValue = parseFloat($('th .nowrap:nth-child(1)').html().replace(/,/g,''));
+    var buyValue = parseFloat($('th :nth-child(3)').html().replace(/,/g,''));
+    var volume = $.shipmentVolume();
+    var sellCollat = sellValue * $.COLLAT_AMOUNT;
+    var buyCollat = buyValue * $.COLLAT_AMOUNT;
+
+    $('#a2NoCollatReward').text($.formatNumberToString(volume * $.A2_COST));
+    $('#a2SellReward').text($.formatNumberToString(volume * $.A2_COST + sellCollat));
+    $('#a2BuyReward').text($.formatNumberToString(volume * $.A2_COST + buyCollat));
+
+    $('#uvhoNoCollatReward').text($.formatNumberToString(volume * $.UVHO_COST));
+    $('#uvhoSellReward').text($.formatNumberToString(volume * $.UVHO_COST + sellCollat));
+    $('#uvhoBuyReward').text($.formatNumberToString(volume * $.UVHO_COST + buyCollat));
+
+    $('#localNoCollatReward').text($.formatNumberToString(volume * $.LOCAL_COST));
+    $('#localSellReward').text($.formatNumberToString(volume * $.LOCAL_COST + sellCollat));
+    $('#localBuyReward').text($.formatNumberToString(volume * $.LOCAL_COST + buyCollat));
+
+    $('.sellAmount').each(function(i, val){
+      $(val).text($.formatNumberToString(sellValue));
+    });
+
+    $('.buyAmount').each(function(i, val){
+      $(val).text($.formatNumberToString(buyValue));
+    });
+  }
 
   $('.copyable').click(function() {
     $(this).text(parseFloat($(this).text().replace(/,/g,'')));
@@ -66,7 +411,14 @@ $(function() {
         $(v).text($.formatNumber($(this).text()));
       }
     });
-  }).blur(function() {
-    alert('sss');
   });
+
+  $('#package-toggle').click($.computePackagedVolumes);
+  // Once everything is set up, we need to actually compute it.
+  $.computeShippingCosts();
+
+  if($.isPackageableShips()) {
+    $('#package-toggle').show();
+    $('#ships-warning').show();
+  }
 });
